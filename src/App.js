fe86264e19f089278302c00671ff160268b5fe08
onkar2405 <onkarvaidyad789@gmail.com>
2025-03-04 23:41:43 +0530

    Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Header from "./components/header/Header";
+import AppBody from "./components/main/AppBody";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route element={<AppBody />} path="/" />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/main/AppBody.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const Body = styled.div``;
+
+export default function AppBody() {
+  return <Body></Body>;
+}
